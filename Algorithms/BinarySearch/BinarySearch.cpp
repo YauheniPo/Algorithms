b5a84274d32b0b3b9b7a64bd087eb8eddb889db3
Yauheni Papovich <ip.popovich@mail.ru>
2017-09-07 02:10:32 +0300

    Corrected BinarySearch

--- Algorithms/BinarySearch/BinarySearch.cpp
+++ Algorithms/BinarySearch/BinarySearch.cpp
@@ -5,6 +5,7 @@
 
 void Init_arr(int arr[], int N);
 void Print_arr(int arr[], int N);
+void Sort_arr(int arr[], int N);
 
 void main(void) {
 	const int N1 = 5, N2 = 7;
@@ -14,14 +15,14 @@ void main(void) {
 
 	srand(time(0));
 	Init_arr(arr1, N1);
-	Init_arr(arr2, N2);
-
 	Print_arr(arr1, N1);
-	
 	printf("\n\n");
 
+	Init_arr(arr2, N2);
 	Print_arr(arr2, N2);
-
+	printf("\n");
+	Sort_arr(arr2, N2);
+	Print_arr(arr2, N2);
 	printf("\n\n");
 
 	for(int i = 0; i < N1; ++i) {
@@ -57,5 +58,23 @@ void Init_arr(int arr[], int N) {
 void Print_arr(int arr[], int N) {
 	for(int i = 0; i < N; ++i) {
 		printf("[%d] = %-4d", i, arr[i]);
+	}
+}
+
+void Sort_arr(int *arr, int N) {
+	int buf;
+	int k1;
+	for(int k = N/2; k > 0; k /= 2) {
+		do {
+			k1 = 0;
+			for(int i = 0, j = k; j < N; ++i, ++j) {
+				if(arr[i] > arr[j]) {
+					buf = arr[i];
+					arr[i] = arr[j];
+					arr[j] = buf;
+					++k1;
+				}
+			}
+		} while(k1);
 	}
 }
